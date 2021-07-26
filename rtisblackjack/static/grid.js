6ec89cf2ecaec3740b0dbd9384bcdb1f8e9badaa
CyberSamurai0 <36649127+CyberSamurai0@users.noreply.github.com>
2021-07-25 23:12:50 -0700

    Update grid.js

--- rtisblackjack/static/grid.js
+++ rtisblackjack/static/grid.js
@@ -50,7 +50,7 @@ function addSignalGroup(intersectionId) {
         let item = document.createElement("div");
         item.className = `grid-item ${sigGroup}`;
         group.appendChild(item);
-        item.innerHTML = `<img class="state-img" src="/blackjack/static/state_icon_${state}.png" alt="${state}"/><h4>Group ${parseInt(sigGroup) + 1}: ${englishState(state)}</h4>`
+        item.innerHTML = `<img class="state-img" src="/static/state_icon_${state}.png" alt="${state}"/><h4>Group ${parseInt(sigGroup) + 1}: ${englishState(state)}</h4>`
     }
 
     let nav = document.createElement("div");
@@ -77,7 +77,7 @@ function updateSignalGroup(intersectionId) {
         for (let i in items) {
             let item = items[i];
             if (item.className && item.className.includes(sigGroup)) {
-                item.innerHTML = `<img class="state-img" src="/blackjack/static/state_icon_${state}.png" alt="${state}"/><h4>Group ${parseInt(sigGroup) + 1}: ${englishState(state)}</h4>`
+                item.innerHTML = `<img class="state-img" src="/static/state_icon_${state}.png" alt="${state}"/><h4>Group ${parseInt(sigGroup) + 1}: ${englishState(state)}</h4>`
             }
         }
     }
